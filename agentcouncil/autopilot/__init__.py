05b1f6d608be802e7277038d898ad011bddf81e2
Kiran Krishna <265731641+kiran-agentic@users.noreply.github.com>
2026-04-15 17:39:41 +1000

    feat(28-01): re-export GateNormalizer from autopilot package
    - Add normalizer wildcard import and _normalizer_all to __all__
    - GateNormalizer importable from agentcouncil.autopilot
    - Full test suite: 790 passed, 0 regressions

--- agentcouncil/autopilot/__init__.py
+++ agentcouncil/autopilot/__init__.py
@@ -2,5 +2,7 @@ from agentcouncil.autopilot.artifacts import *  # noqa: F401, F403
 from agentcouncil.autopilot.artifacts import __all__ as _artifacts_all  # noqa: F401
 from agentcouncil.autopilot.loader import *  # noqa: F401, F403
 from agentcouncil.autopilot.loader import __all__ as _loader_all  # noqa: F401
+from agentcouncil.autopilot.normalizer import *  # noqa: F401, F403
+from agentcouncil.autopilot.normalizer import __all__ as _normalizer_all  # noqa: F401
 
-__all__ = [*_artifacts_all, *_loader_all]
+__all__ = [*_artifacts_all, *_loader_all, *_normalizer_all]
