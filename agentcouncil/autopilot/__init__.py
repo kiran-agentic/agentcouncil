37ef92276dc5755eb3a02c04be7bc85c5de67086
Kiran Krishna <265731641+kiran-agentic@users.noreply.github.com>
2026-04-15 18:00:17 +1000

    feat(29-01): re-export run.py public API from autopilot package
    - Add run.* wildcard export and _run_all alias
    - Update __all__ to include _run_all alongside existing exports

--- agentcouncil/autopilot/__init__.py
+++ agentcouncil/autopilot/__init__.py
@@ -4,5 +4,7 @@ from agentcouncil.autopilot.loader import *  # noqa: F401, F403
 from agentcouncil.autopilot.loader import __all__ as _loader_all  # noqa: F401
 from agentcouncil.autopilot.normalizer import *  # noqa: F401, F403
 from agentcouncil.autopilot.normalizer import __all__ as _normalizer_all  # noqa: F401
+from agentcouncil.autopilot.run import *  # noqa: F401, F403
+from agentcouncil.autopilot.run import __all__ as _run_all  # noqa: F401
 
-__all__ = [*_artifacts_all, *_loader_all, *_normalizer_all]
+__all__ = [*_artifacts_all, *_loader_all, *_normalizer_all, *_run_all]
